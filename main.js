318fbcc937dcae1ee015525a09d1b9e5fcc9f23f
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-04-03 17:45:53 +0700

    event on

--- main.js
+++ main.js
@@ -4,16 +4,27 @@ var vueInstance = new Vue({
         title: 'dien thoai',
         main: 'test',
         url: "https://v2.vuejs.org/v2/guide/",
-        message: "test method"
+        message: "test method",
+        counter: 0,
+        clientX: 0,
+        clientY: 0,
     },
     // them ham
     methods: {
-        say: function(text) {
+        say: function (text) {
             return 'hello' + text;
         },
         reverseMessage: function () {
             this.message = this.message.split('').reverse().join('')
-          }
+        },
+        handleClick(number,e) {
+            console.log('click', e.target);
+            this.counter += number
+        },
+        handleMouseMove(e) {
+            this.clientX = e.clientX
+            this.clientY = e.clientY
+        }
     }
 })
 console.log(vueInstance)
