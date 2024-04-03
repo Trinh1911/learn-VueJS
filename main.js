f463042e8d7e68d6e2b65e49d23a594641f46eff
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2024-04-03 17:19:55 +0700

    su dung this trong vue

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ var vueInstance = new Vue({
     data: {
         title: 'dien thoai',
         main: 'test',
-        url: "https://v2.vuejs.org/v2/guide/"
+        url: "https://v2.vuejs.org/v2/guide/",
+        message: "test method"
     },
     // them ham
     methods: {
         say: function(text) {
             return 'hello' + text;
-        }
+        },
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+          }
     }
 })
 console.log(vueInstance)
